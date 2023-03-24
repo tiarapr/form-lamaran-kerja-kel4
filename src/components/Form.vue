f3df1e575de1daaafe6293e3035ce27f6d75a299
tiarapr <tiaraputriramadhani1@gmail.com>
2023-03-24 10:15:13 +0700

    update comment

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,10 @@
-<script setup>
-import TableData from './TableData.vue'
-</script>
-
 <template>
     <div class="container">
         <div class="row">
             <div>
                 <form class="lamaran-form" action="#">
                     <h1 class="border-bottom mb-8">Form Lamaran Kerja</h1>
+                    <!-- Base Input Nama -->
                     <base-input type="text" v-model="form.name" label="Nama"></base-input>
                     <div class="form-group">
                         <label>Deskripsi Diri</label>
@@ -19,27 +16,35 @@ import TableData from './TableData.vue'
                         <textarea class="form-control" rows="5" :value="form.experience"
                             @input="form.experience = $event.target.value"></textarea>
                     </div>
+                    <!-- Base Select Pendidikan -->
                     <base-select label="Pendidikan" v-model="form.education" :options="educations"
                         placeholder="Pilih Pendidikan Terakhir">
                     </base-select>
+                    <!-- Base Input Email -->
                     <base-input type="email" v-model="form.email" label="Email"></base-input>
+                    <!-- Base Input No. Telp -->
                     <base-input type="text" v-model="form.telp" label="No. Telp"></base-input>
                     <div class="form-group">
                         <label>Alamat</label>
                         <textarea class="form-control" rows="5" :value="form.address"
                             @input="form.address = $event.target.value"></textarea>
                     </div>
+                    <!-- Base Input Tanggal Lahir -->
                     <base-input type="date" v-model="form.tgl_lahir" label="Tanggal Lahir"></base-input>
+                    <!-- Base Radio Group Jenis Kelamin -->
                     <div class="base-group">
                         <base-radio-group label="Jenis Kelamin" name="jenisKelamin" :options="jk" v-model="form.jk">
                         </base-radio-group>
                     </div>
+                    <!-- Base Select Posisi -->
                     <base-select label="Pilih Posisi" v-model="form.posisiId" :options="positions"
                         placeholder="Select a position">
                     </base-select>
+                    <!-- Base Checkbox Group Skills -->
                     <base-checkbox-group label="Skills" v-model="form.skills" :options="skills" inline>
                     </base-checkbox-group>
                     <hr />
+                    <!-- Button Save -->
                     <div class="form-group">
                         <button class="btn btn-lg btn-primary">Save</button>
                     </div>
@@ -49,49 +54,49 @@ import TableData from './TableData.vue'
     </div>
 </template>
 <script>
-export default {
-    data() {
-        return {
-            form: {
-                name: "",
-                description: "",
-                experience: "",
-                email: "",
-                telp: "",
-                address: "",
-                tgl_lahir: "",
-                education: "",
-                jk: "",
-                posisiId: "",
-                skills: [],
-            },
-            educations: [
-                { value: 1, text: "SMA / SMK" },
-                { value: 2, text: "D3" },
-                { value: 3, text: "D4 / S1" },
-                { value: 4, text: "S2" },
-            ],
-            positions: [
-                { value: 1, text: "Front End Developer" },
-                { value: 2, text: "Back End Developer" },
-                { value: 3, text: "Full Stack Developer" },
-                { value: 4, text: "UI/UX Designer" },
-            ],
-            jk: [
-                { text: "Laki - Laki", value: 1 },
-                { text: "Perempuan", value: 2 },
-            ],
-            skills: [
-                { value: 1, text: "HTML, CSS, JavaScript" },
-                { value: 2, text: "Vue JS" },
-                { value: 3, text: "React JS" },
-                { value: 4, text: "PHP, MySQL" },
-                { value: 5, text: "Laravel" },
-                { value: 6, text: "Code Igniter" },
-                { value: 7, text: "UI Design" },
-                { value: 8, text: "UX Design" },
-            ],
-        };
-    },
-};
+    export default {
+        data() {
+            return {
+                form: {
+                    name: "",
+                    description: "",
+                    experience: "",
+                    email: "",
+                    telp: "",
+                    address: "",
+                    tgl_lahir: "",
+                    education: "",
+                    jk: "",
+                    posisiId: "",
+                    skills: [],
+                },
+                educations: [
+                    { value: 1, text: "SMA / SMK" },
+                    { value: 2, text: "D3" },
+                    { value: 3, text: "D4 / S1" },
+                    { value: 4, text: "S2" },
+                ],
+                positions: [
+                    { value: 1, text: "Front End Developer" },
+                    { value: 2, text: "Back End Developer" },
+                    { value: 3, text: "Full Stack Developer" },
+                    { value: 4, text: "UI/UX Designer" },
+                ],
+                jk: [
+                    { text: "Laki - Laki", value: 1 },
+                    { text: "Perempuan", value: 2 },
+                ],
+                skills: [
+                    { value: 1, text: "HTML, CSS, JavaScript" },
+                    { value: 2, text: "Vue JS" },
+                    { value: 3, text: "React JS" },
+                    { value: 4, text: "PHP, MySQL" },
+                    { value: 5, text: "Laravel" },
+                    { value: 6, text: "Code Igniter" },
+                    { value: 7, text: "UI Design" },
+                    { value: 8, text: "UX Design" },
+                ],
+            };
+        },
+    };
 </script>
