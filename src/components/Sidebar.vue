8264910d93672e2125c326abfc8727dfb92dfe61
tiarapr <tiaraputriramadhani1@gmail.com>
2023-04-18 20:36:10 +0700

    menambahkan active class di sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,32 +2,26 @@
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
             <div> 
-                <router-link :to="{name: 'home'}">
-                <a href="#" class="nav_logo"> 
+                <router-link :to="{name: 'home'}" class="nav_logo">
                     <span class="nav_logo-name">MyCareer
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stars" viewBox="0 0 16 16">
                             <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
                         </svg>
                     </span> 
-                </a>
                 </router-link>
                 <div class="nav_list">
-                    <router-link :to="{name: 'create-data'}">
-                    <a class="nav_link"> 
+                    <router-link :to="{name: 'create-data'}" class="nav_link" active-class="active">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
                             <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
                             <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
                         </svg>
                         <span class="nav_name">Form</span> 
-                    </a> 
                     </router-link>
-                    <router-link :to="{name: 'data'}">
-                    <a href="#" class="nav_link"> 
+                    <router-link :to="{name: 'data'}" class="nav_link" active-class="active">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
                             <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
                         </svg> 
                         <span class="nav_name">Data Pelamar</span> 
-                    </a>
                     </router-link>
                 </div>
             </div>
@@ -62,10 +56,6 @@ body {
     font-family: var(--body-font);
     font-size: var(--normal-font-size);
     transition: .5s
-}
-
-a {
-    text-decoration: none
 }
 
 .l-navbar {
@@ -122,29 +112,14 @@ a {
     font-size: 1.25rem
 }
 
-.show {
-    left: 0
-}
-
 .body-pd {
     padding-left: calc(var(--nav-width) + 1rem)
 }
 
 .active {
-    color: var(--white-color)
-}
-
-.active::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    width: 2px;
-    height: 32px;
-    background-color: var(--white-color)
-}
-
-.height-100 {
-    height: 100vh
+    color: var(--white-color);
+    font-size: 18px;
+    font-weight: 700;
 }
 
 @media screen and (min-width: 768px) {
@@ -153,22 +128,9 @@ a {
         padding-left: calc(var(--nav-width) + 2rem)
     }
 
-    .header {
-        height: calc(var(--header-height) + 1rem);
-        padding: 0 2rem 0 calc(var(--nav-width) + 2rem)
-    }
-
     .l-navbar {
         left: 0;
         padding: 1rem 1rem 0 0
-    }
-
-    .show {
-        width: calc(var(--nav-width) + 156px)
-    }
-
-    .body-pd {
-        padding-left: calc(var(--nav-width) + 188px)
     }
 }
 </style>
